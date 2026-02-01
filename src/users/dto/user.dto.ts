db150b03c02ec4963a44a56a2f90af2b150593d8
Mohammad Al Bast <mohammadbast4@gmail.com>
2026-02-01 12:41:21 +0200

    Add class-validator and class-transformer dependencies; refactor UserDto and UsersController for validation

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,6 +1,34 @@
-export type UserDto = {
+import { IsEmail, IsInt, IsNumber, IsString, max, min } from 'class-validator';
+
+export class UserDto {
+  @IsNumber()
   id: number;
+  @IsString()
   name: string;
+  @IsEmail()
   email: string;
+  @IsInt()
   age: number;
-};
+}
+
+export class createUserDto {
+  @IsString()
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsInt()
+  @min(0)
+  @max(80)
+  age: number;
+}
+
+export class updateUserDto {
+  @IsString()
+  name?: string;
+  @IsEmail()
+  email?: string;
+  @IsInt()
+  @min(0)
+  @max(80)
+  age?: number;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
-import { UserDto } from './dto/user.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserDto, createUserDto, updateUserDto } from './dto/user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -14,17 +26,53 @@ export class UsersController {
     },
   ];
   @Get()
-  getAllUsers() {
+  getAllUsers(@Query('name') name: string) {
+    if (name) {
+      return this.users.filter((user) => user.name == name);
+    }
     return this.users;
   }
 
+  @Get(':id')
+  getUserById(@Param('id') id: number) {
+    const user = this.users.find((user) => user.id === Number(id));
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+
   @Post()
-  createUser() {
-    return { id: 3, name: 'New User' };
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() createUserDto: createUserDto) {
+    const newUser: UserDto = {
+      id: this.users.length + 1,
+      ...createUserDto,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: number, @Body() updateUserDto: updateUserDto) {
+    const userIndex = this.users.findIndex((user) => user.id === Number(id));
+    if (!this.users[userIndex]) {
+      throw new NotFoundException();
+    }
+    this.users[userIndex] = { ...this.users[userIndex], ...updateUserDto };
+    return this.users[userIndex];
   }
 
   @Delete(':id')
-  deleteUser() {
+  deleteUser(@Param('id') id: number, @Body() body: updateUserDto) {
+    const userIndex = this.users.findIndex((user) => user.id === Number(id));
+    if (!this.users[userIndex]) {
+      throw new NotFoundException();
+    }
+    this.users[userIndex].name = body.name || this.users[userIndex].name;
+    this.users[userIndex].email = body.email || this.users[userIndex].email;
+    this.users[userIndex].age = body.age || this.users[userIndex].age;
+    this.users.splice(userIndex, 1);
     return { message: 'User deleted successfully' };
   }
 }
