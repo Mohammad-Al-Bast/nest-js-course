3236afa067f3bdd0d3f5237db7c64ca5da008ed7
Mohammad Al Bast <mohammadbast4@gmail.com>
2026-02-08 09:48:44 +0200

    Implement CRUD operations in ProductsController and enhance ProductsService with error handling

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { createProductDto } from './dtos/createProduct.dto';
+import { updateProductDto } from './dtos/updateProduct.dto';
 
 @Controller('products') //path name
 export class ProductsController {
@@ -7,5 +9,25 @@ export class ProductsController {
   @Get()
   getAllProducts() {
     return this.productService.getAllProducts();
+  }
+
+  @Get(':id')
+  getProductById() {
+    return this.productService.getProductById(1);
+  }
+
+  @Post()
+  create(@Body() dto: createProductDto) {
+    return this.productService.create(dto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() dto: updateProductDto) {
+    return this.productService.update(id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.productService.delete(id);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/products.entity';
+import { Repository } from 'typeorm';
+import { createProductDto } from './dtos/createProduct.dto';
+import { updateProductDto } from './dtos/updateProduct.dto';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private repo: Repository<Product>,
+  ) {}
   public getAllProducts() {
-    return [];
+    return this.repo.find();
+  }
+
+  async getProductById(id: number) {
+    const product = await this.repo.findOneBy({ id });
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return product;
+  }
+
+  async create(dto: createProductDto) {
+    const product = this.repo.create(dto);
+    await this.repo.save(product);
+    return product;
+  }
+
+  async update(id: number, dto: updateProductDto) {
+    const product = await this.getProductById(id);
+    product.name = dto.name ?? product.name;
+    product.description = dto.description ?? product.description;
+    product.price = dto.price ?? product.price;
+    await this.repo.save(product);
+    return product;
+  }
+
+  async delete(id: number) {
+    await this.getProductById(id);
+    await this.repo.softDelete({ id });
   }
 }
